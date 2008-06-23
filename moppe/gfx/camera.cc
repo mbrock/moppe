7840c89b6bdee6539e67aa7d70accae85afb6e0a
Mikael Brockman <phubuh@gmail.com>
2008-06-23 21:23:14 +0200

    Some camera stuff.

--- moppe/gfx/camera.cc
+++ moppe/gfx/camera.cc
@@ -1,1 +1,10 @@
 #include <moppe/gfx/camera.hh>
+
+namespace moppe {
+namespace gfx {
+  void Camera::realize ()
+  {
+    
+  }
+}
+}
--- moppe/gfx/camera.hh
+++ moppe/gfx/camera.hh
@@ -8,10 +8,17 @@ namespace moppe {
 namespace gfx {
   class Camera {
   public:
-    Camera () { }
+    Camera (Vector3D<float> position, Vector3D<float> target)
+      : m_position (position),
+	m_target (target)
+    { }
+
+    void realize ();
+    void set_pitch_and_yaw (float pitch, float yaw);
 
   private:
-    
+    Vector3D<float> m_position;
+    Vector3D<float> m_target;
   };
 }
 }
